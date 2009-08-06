ed56ec957d3bde8c75f5b9fadcf89b1c8d81322a
swv <daveyduckdog@hotmail.com>
2009-08-06 20:18:32 +0100

    updated gff.h for correct syntax and ints for the number of elements in pointer arrays

--- gff.h
+++ gff.h
@@ -1,37 +1,44 @@
 typedef enum {Inheritance, Partof, Root} Strand_t;
 
 typedef struct {
-    int start,
-    int end,
-    Strand_t strand,
-    int phase
+  int start;
+  int end;
+  //  Strand_t strand;
+  int strand;
+  int phase;
 } SeqLoc;
 
 // Duplicates ID, Name, parents
 typedef struct {
-    char *tag,
-    char **vals
+  char *tag;
+  char **vals;
+  int num_vals;
 } Attribute;
 
 typedef struct {
-    char *seqid,
-    char *source,
-    char *type,
-    SeqLoc **locs,
-    float score,
-    char *ID,
-    char *Name,
-    char **parents,
-    Attribute **attributes
+  char *seqid;
+  char *source;
+  char *type;
+  int num_locs;
+  SeqLoc **locs;
+  float score;
+  char *ID;
+  char *Name;
+  int num_parents;
+  char **parents;
+  int num_attributes;
+  Attribute **attributes;
+  //  char **attributes;
 } Feature;
 
 typedef struct {
-  char *ID,
-  char *seqstr
+  char *ID;
+  char *seqstr;
 } Seq;
 
 typedef struct {
-  Attribute **attributes,
-  Feature **features,
-  Seq **seqs
+  Attribute **attributes;
+  int num_features;
+  Feature **features;
+  Seq **seqs;
 } GFFDoc;
